6738041a53d753825a879c8f4097f64e80881726
suicho <chosujin84@gmail.com>
2016-02-17 22:46:13 -0800

    GET GEO LOCATION
    - Add navigator to get geo location

---
+++ Intermediate Front End Development Projects/Local Weather App/js/index.js
@@ -0,0 +1,5 @@
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(function(position){
+    $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
