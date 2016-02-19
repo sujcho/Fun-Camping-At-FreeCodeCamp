fc33bb4a9a1d5a8955c9c613ed2dd5d3dda6e525
suicho <chosujin84@gmail.com>
2016-02-18 22:04:42 -0800

    Link JQuery

--- Intermediate Front End Development Projects/Local Weather App/js/index.js
+++ Intermediate Front End Development Projects/Local Weather App/js/index.js
@@ -1,5 +1,12 @@
 if(navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(function(position){
+navigator.geolocation.getCurrentPosition(function(position) {
     $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
   });
 }
+
+$(document).ready(function(){
+$.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q=Montpellier&mode=json&units=metric&cnt=10",function(result){
+    alert("City: "+result.city.name);
+    alert("Weather: "+ result.list[0].weather[0].description);
+    });
+});
