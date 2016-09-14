dd52b59d6967ff6e0ca81008a2830a65a875066f
sujcho <chosujin84@gmail.com>
2016-09-13 17:03:19 -0700

    SHOW CURRENT LOCATION
    -Retrieved current location data

--- Intermediate Front End Development Projects/Local Weather App/js/index.js
+++ Intermediate Front End Development Projects/Local Weather App/js/index.js
@@ -1,12 +1,7 @@
-if(navigator.geolocation){
-navigator.geolocation.getCurrentPosition(function(position) {
-    $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-  });
-}
-
 $(document).ready(function(){
-$.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q=Montpellier&mode=json&units=metric&cnt=10",function(result){
-    alert("City: "+result.city.name);
-    alert("Weather: "+ result.list[0].weather[0].description);
-    });
+  $.getJSON('https://freegeoip.net/json/',function(json){
+      $("#city").html(json.city);
+      $("#region").html(json.region_name);
+      $("#country").html(json.country_name);
+  });
 });
