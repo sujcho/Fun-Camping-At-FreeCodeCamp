0ebf129ce8ad5436961ce2be317748bebbadd71a
sujcho <chosujin84@gmail.com>
2016-09-14 20:48:40 -0700

    FIXED GETTING LOCATION DATA
    -Change getJSON to Ajax api
    -Store location data to var and return it

--- Intermediate Front End Development Projects/Local Weather App/js/index.js
+++ Intermediate Front End Development Projects/Local Weather App/js/index.js
@@ -1,7 +1,57 @@
+var app_id = '&APPID='+'942f5f77f243a4c6059a46b5523bc3e1';
+var openweather = 'api.openweathermap.org/data/2.5/weather?';
 $(document).ready(function(){
-  $.getJSON('https://freegeoip.net/json/',function(json){
+     var loc = ajax_getLocInfo();
+     var lat = 'lat=' + String(loc.latitude);
+     var lon = '&lon=' + String(loc.longitude);
+     var url = openweather + lat + lon + app_id;
+    // ajax_getWeatherInfo(url);
+  /*
+   $('#weather').text('add');
+    $.getJSON(url, function(data){
+      $('#weather').text('add');
+      $("#weather").html(JSON.stringify(data));
+    })
+    */
+});
+
+function getLocInfo(){
+     $.getJSON('https://freegeoip.net/json/',function(json){
+    //  $("#message").html(JSON.stringify(json));
       $("#city").html(json.city);
       $("#region").html(json.region_name);
       $("#country").html(json.country_name);
+    });
+}
+
+function ajax_getLocInfo(){
+  var global ={};
+  $.ajax({
+    async: false,
+    url:'https://freegeoip.net/json/',
+    success: function(json){
+     // $("#message").html(JSON.stringify(json));
+      global = json;
+      $("city").html(json.city);
+      $("#region").html(json.region_name);
+      $("#country").html(json.country_name);
+      var lat = 'lat=' + String(json.latitude);
+      var lon = '&lon=' + String(json.longitude);
+      var url = openweather + lat + lon + app_id;
+      ajax_getWeatherInfo(url);
+    }
   });
-});
+  return global;
+}
+
+
+function ajax_getWeatherInfo(url){
+  $.ajax({
+    async: false,
+    url:url,
+    success: function(json){
+      alert('test');
+      $("#weather").html(json.weather);
+    }
+  });
+}
